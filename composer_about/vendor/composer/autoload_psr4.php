849a6d308455cb8da08aed35f1afe345e6590c27
lifeifei <lifeifei@olmail.org>
2017-06-07 18:12:34 +0800

    composer下来symfony/routing

--- composer_about/vendor/composer/autoload_psr4.php
+++ composer_about/vendor/composer/autoload_psr4.php
@@ -6,6 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Symfony\\Component\\Routing\\' => array($vendorDir . '/symfony/routing'),
     'Psr\\Log\\' => array($vendorDir . '/psr/log/Psr/Log'),
     'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
 );
