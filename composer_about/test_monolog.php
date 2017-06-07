49b64979b7f5b8d6557da94d2886c156223a8f69
lifeifei <lifeifei@olmail.org>
2017-06-07 16:13:38 +0800

    composer只有monolog时

---
+++ composer_about/test_monolog.php
@@ -0,0 +1,13 @@
+<?php
+require_once "vendor/autoload.php";
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('path/to/your.log', Logger::WARNING));
+
+// add records to the log
+$log->warning('c');
+$log->error('lan');
